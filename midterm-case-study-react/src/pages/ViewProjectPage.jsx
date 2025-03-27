7964a2d2dfac8045d9cdb770d76c2c12eb3964d4
gab <jardingabrielle31@gmail.com>
2025-03-27 15:44:46 +0800

    added css

--- midterm-case-study-react/src/pages/ViewProjectPage.jsx
+++ midterm-case-study-react/src/pages/ViewProjectPage.jsx
@@ -2,12 +2,11 @@ import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 // Bootstrap
 import Dropdown from "react-bootstrap/Dropdown";
-import Button from "../components/Button.jsx";
 // CSS styles
 import Style from "../css modules/ProjectDashboard.module.css";
 // FontAwesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faPlus, faTasks, faChartBar } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faPlus, faTasks, faChartBar, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 // Components
 import ProjectList from "../components/ProjectList.jsx";
 
@@ -34,64 +33,50 @@ export default function ViewProjectPage() {
     navigate("/", { replace: true });
   }
 
-  function handleCreateProject() {
-    navigate("/create-project");
-  }
-
-  function handleAssignTasks() {
-    navigate("/assign-tasks");
-  }
-
-  function handleTrackProgress() {
-    navigate("/track-progress");
-  }
-
-  function handleViewReports() {
-    navigate("/reports");
+  function handleNavigation(route) {
+    navigate(route);
   }
 
   return (
-    <section>
-      <header>
-        <div className={Style.header__Title}>
-          <div className={Style.titleLayout}>
-            <h1>Project Management System</h1>
-          </div>
+    <section className={Style.container}>
+      {/* Sidebar */}
+      <aside className={Style.sidebar}>
+        <h2>Dashboard</h2>
+        <ul className={Style.sidebarMenu}>
+          <li onClick={() => handleNavigation("/create-project")}>
+            <FontAwesomeIcon icon={faPlus} /> Create Project
+          </li>
+          <li onClick={() => handleNavigation("/assign-tasks")}>
+            <FontAwesomeIcon icon={faTasks} /> Assign Tasks
+          </li>
+          <li onClick={() => handleNavigation("/track-progress")}>
+            <FontAwesomeIcon icon={faChartBar} /> Track Progress
+          </li>
+          <li onClick={() => handleNavigation("/reports")}>
+            <FontAwesomeIcon icon={faChartBar} /> View Reports
+          </li>
+          <li onClick={handleLogout}>
+            <FontAwesomeIcon icon={faSignOutAlt} /> Log Out
+          </li>
+        </ul>
+      </aside>
 
+      {/* Main content */}
+      <main className={Style.mainContent}>
+        <header className={Style.header}>
+          <h1>Project Management System</h1>
           <Dropdown className={Style.header__userIcon}>
-            <Dropdown.Toggle
-              className={Style.iconBorder}
-              variant="link"
-              id="dropdown-basic"
-            >
-              <FontAwesomeIcon
-                icon={faUser}
-                size="lg"
-                style={{ color: "#4470FE" }}
-              />
+            <Dropdown.Toggle className={Style.iconBorder} variant="link" id="dropdown-basic">
+              <FontAwesomeIcon icon={faUser} size="lg" style={{ color: "#4470FE" }} />
             </Dropdown.Toggle>
-
-            <Dropdown.Menu className="m-0 p-0">
-              <Button
-                className="d-flex justify-content-center align-items-center dropdown-item m-0 p-0"
-                name={"Log Out"}
-                onClick={handleLogout}
-              />
-            </Dropdown.Menu>
           </Dropdown>
-        </div>
+        </header>
 
         <h2>Project List</h2>
-
-        <div className={Style.adminActions}>
-          <Button name="Create Project" onClick={handleCreateProject} />
-          <Button name="Assign Tasks" onClick={handleAssignTasks} />
-          <Button name="Track Progress" onClick={handleTrackProgress} />
-          <Button name="View Reports" onClick={handleViewReports} />
+        <div className={Style.projectList}>
+          <ProjectList projects={projects} />
         </div>
-
-        <ProjectList projects={projects} />
-      </header>
+      </main>
     </section>
   );
 }
